2165042a0d8192ed9e545e4f2583309248ddee07
Nguyễn Thanh Hiếu <hieu.nt.64cntt@ntu.edu.vn>
2025-02-11 19:19:38 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,9 @@
+package tiil.edu;
+
+public class XinChao {
+
+	public static void main(String[] args) {
+		System.out.println("TÔi lần đầu học Java, và đang thử nghiệm quản lý mã nguồn với Git");
+	}
+
+}
