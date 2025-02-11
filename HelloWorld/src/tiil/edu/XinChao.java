ab2e5e1d71d1c2b65a11bd8a66d8c17256697aff
Nguyễn Thanh Hiếu <hieu.nt.64cntt@ntu.edu.vn>
2025-02-11 19:24:03 +0700

    Cập nhập code
    Sửa mới và thêm dòng

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -3,7 +3,8 @@ package tiil.edu;
 public class XinChao {
 
 	public static void main(String[] args) {
-		System.out.println("TÔi lần đầu học Java, và đang thử nghiệm quản lý mã nguồn với Git");
+		System.out.println("TÔi lần đầu học JAVA, và đang thử nghiệm quản lý mã nguồn với Git");
+		System.out.print("Tôi thêm một dòng, và sửa lại chữ Java ở trên");
 	}
 
 }
