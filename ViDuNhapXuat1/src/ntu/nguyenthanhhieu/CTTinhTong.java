a6c8050a5fd1e3298502bb65284d4e8a2232ec7e
Nguyễn Thanh Hiếu <hieu.nt.64cntt@ntu.edu.vn>
2025-02-18 09:54:44 +0700

    Full code

--- ViDuNhapXuat1/src/ntu/nguyenthanhhieu/CTTinhTong.java
+++ ViDuNhapXuat1/src/ntu/nguyenthanhhieu/CTTinhTong.java
@@ -1,9 +1,25 @@
 package ntu.nguyenthanhhieu;
 
+import java.util.Scanner;
+
 public class CTTinhTong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//in ra mot dong tieu de app
+		System.out.print("CT Tinh Tong \n");
+		//chuan bi cho viec nhap 
+		Scanner banPhim = new Scanner (System.in);
+		//In dong huong nhan Moi nhap A
+		System.out.println("Moi nhap a:");
+		double a= banPhim.nextDouble();
+		System.out.println("Moi nhap b:");
+		double b= banPhim.nextDouble();
+		//Tinh toan
+		double  tong = a+b;
+		
+		System.out.print("Tong la:");
+		System.out.print(tong);
+		
 
 	}
 
